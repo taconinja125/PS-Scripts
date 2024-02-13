7d4a5d29b6c07d508e03f3761e6d1a126579a783
Josh Perez <josh-perez11@outlook.com>
2024-02-13 13:43:08 -0800

    Added check for elevated permissions

--- Start-AssetInventory.ps1
+++ Start-AssetInventory.ps1
@@ -18,14 +18,29 @@ param(
     [string]$OutFile = "~\inventoryReport.json"
 )
 
+# Are you running this elevated?
+if ($IsWindows) {
+    $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $windowsPrincipal = New-Object Security.Principal.WindowsPrincipal($currentUser)
+    $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+
+    if (-not $windowsPrincipal.IsInRole($adminRole)) {
+        Write-Error "This script must be run with administrative privileges"
+        return
+    }
+} elseif ($([System.Environment]::UserName) -ne "root") {
+        Write-Error "This script must be run as root"
+        return
+}
+
 #region System info
 $systemInfo = @{
-    OSPlatform   = ($IsWindows) ? "Windows" : "macOS"
     OSVersion    = [System.Environment]::OSVersion.Version.ToString()
+    OSPlatform   = ($IsWindows) ? "Windows" : "macOS"
     MachineName  = ($IsWindows) ? $env:COMPUTERNAME : (/bin/hostname -s)
-    SerialNumber = ($IsWindows) ? (Get-CimInstance -ClassName Win32_BIOS).SerialNumber : ((ioreg -l | grep IOPlatformSerialNumber).split(' ')[-1].trim('"'))
-    CPU          = ($IsWindows) ? (Get-WmiObject -Class Win32_Processor).Name : (sysctl -n machdep.cpu.brand_string)
-    RAM          = ($IsWindows) ? ("{0} GB" -f ((Get-WmiObject -Class Win32_ComputerSystem).TotalPhysicalMemory / 1GB -as [int])) : ("{0} GB" -f ((system_profiler SPHardwareDataType | grep "Memory:" | Out-String).Split(":").Trim()[1] -replace " GB", ""))
+    SerialNumber = ($IsWindows) ? (Get-CimInstance -ClassName Win32_BIOS).SerialNumber : ((/usr/sbin/ioreg -l | /usr/bin/grep IOPlatformSerialNumber).split(' ')[-1].trim('"'))
+    CPU          = ($IsWindows) ? (Get-WmiObject -Class Win32_Processor).Name : (/usr/sbin/sysctl -n machdep.cpu.brand_string)
+    RAM          = ($IsWindows) ? ("{0} GB" -f ((Get-WmiObject -Class Win32_ComputerSystem).TotalPhysicalMemory / 1GB -as [int])) : ("{0} GB" -f ((/usr/sbin/system_profiler SPHardwareDataType | /usr/bin/grep "Memory:" | Out-String).Split(":").Trim()[1] -replace " GB", ""))
 }
 #endregion System info
 
@@ -40,7 +55,7 @@ if ($IsWindows) {
             $identifier = /usr/bin/defaults read $(Join-Path -Path $_.FullName -ChildPath /Contents/Info.plist) CFBundleIdentifier
             $version = /usr/bin/defaults read $(Join-Path -Path $_.FullName -ChildPath /Contents/Info.plist) CFBundleShortVersionString
             [PSCustomObject]@{
-                Name = $_.BaseName
+                Name        = $_.BaseName
                 Identifier  = $identifier
                 Version     = $version
             }
@@ -51,8 +66,8 @@ if ($IsWindows) {
 
 #region Disk info
 $diskSpace = Get-PSDrive -PSProvider FileSystem |
-    Where-Object { ($_.Used -ne $null) -and ($_.Name -NE 'Temp') } |
-    Select-Object Name, @{Name = "UsedSize(GB)"; Expression = { [math]::Round($_.Used / 1GB, 2) } }, @{Name = "FreeSpace(GB)"; Expression = { [math]::Round($_.Free / 1GB, 2) } }
+    Where-Object {($_.Used -ne $null) -and ($_.Name -NE 'Temp')} |
+    Select-Object Name, @{Name = "Used(GB)"; Expression = {[math]::Round($_.Used / 1GB, 2)}}, @{Name = "Free(GB)"; Expression = {[math]::Round($_.Free / 1GB, 2)}}
 #endregion Disk info
 
 #region Disk encryption
@@ -66,7 +81,7 @@ if ($IsWindows) {
         }
     }
 } else {
-    $diskEncryption = (fdesetup status | Out-String).trim()
+    $diskEncryption = (/usr/bin/fdesetup status | Out-String).trim()
 }
 #endregion Disk encryption
 
